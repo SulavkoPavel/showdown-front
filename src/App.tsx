ce583d0dd4b1b171fa407a4dbb87d688b0a5cace
RomanKryukoff <kriukov-ra@ic.ntcees.ru>
2024-07-21 16:34:51 +0300

    Navigate to /my-tables when navigate to /

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import LoginPage from "./pages/Login/LoginPage.tsx";
 import RegisterPage from "./pages/Register/RegisterPage.tsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import MyTablesPage from "./pages/MyTables/MyTablesPage.tsx";
 import CreateTablePage from "./pages/CreateTable/CreateTablePage.tsx";
 import TableSettingsPage from "./pages/TableSettings/TableSettingsPage.tsx";
@@ -11,6 +11,7 @@ function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/my-tables" />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/profile-settings" element={<ProfileSettingsPage />} />
