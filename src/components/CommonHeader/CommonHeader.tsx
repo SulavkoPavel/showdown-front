1d5f915f1c7a7c8e332d94f30dbfc81be4bb7a61
RomanKryukoff <kriukov-ra@ic.ntcees.ru>
2024-10-05 15:46:24 +0300

    Make profile menu be always on top and hide it by clicking outside

--- src/components/CommonHeader/CommonHeader.tsx
+++ src/components/CommonHeader/CommonHeader.tsx
@@ -4,7 +4,7 @@ import '../SmallText/small-text.css'
 import './__profile-menu/common-header__profile-menu.css'
 import '../../main-container.css'
 import TextLogo from '../TextLogo/TextLogo.tsx';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import HeaderProfilePhoto from "../HeaderProfilePhoto/HeaderProfilePhoto.tsx";
 import ProfileMenu from "../ProfileMenu/ProfileMenu.tsx";
 
@@ -14,6 +14,23 @@ interface Props {
 
 const CommonHeader = ({gameName}: Props) => {
     const [isProfileMenuVisible, setProfileMenuVisible] = useState(false);
+    const profilePhotoRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                profilePhotoRef.current &&
+                !profilePhotoRef.current.contains(event.target as Node)
+            ) {
+                setProfileMenuVisible(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [profilePhotoRef]);
 
     return (
         <header className='common-header'>
@@ -21,13 +38,16 @@ const CommonHeader = ({gameName}: Props) => {
             <div className='small-text common-header__game-name'>
                 {gameName}
             </div>
-            <HeaderProfilePhoto
+            <div
+                ref={profilePhotoRef}
                 onClick={() => setProfileMenuVisible(!isProfileMenuVisible)}
-            />
-            <ProfileMenu
-                className='common-header__profile-menu'
-                visible={isProfileMenuVisible}
-            />
+            >
+                <HeaderProfilePhoto/>
+                <ProfileMenu
+                    className='common-header__profile-menu'
+                    visible={isProfileMenuVisible}
+                />
+            </div>
         </header>
     )
 }
